a065586d6707afbd57ea00db80a7fcab48b0a7d9
Tyler Wilcox <tywilly300@gmail.com>
2015-04-25 13:13:36 -0400

    Added an Obama sprite :)

---
+++ apcs/entitys/SpriteTest.java
@@ -0,0 +1,27 @@
+package apcs.entitys;
+
+import com.tywilly.WillyEngine.Engine;
+import com.tywilly.WillyEngine.entity.sprite.Sprite;
+import com.tywilly.WillyEngine.texture.Texture;
+import com.tywilly.WillyEngine.update.Updateable;
+
+public class SpriteTest extends Sprite implements Updateable{
+
+	public SpriteTest(int x, int y) {
+		super(x, y, 64,64, new Texture("assets/obama_sprite_left.png"));
+		this.getTexture().loadIntoMemery();
+	}
+
+	@Override
+	public void update(long mili) {
+		// TODO Auto-generated method stub
+		
+		if(xLoc >= Engine.display.getWidth()){
+			xLoc = 0;
+		}
+		
+		xLoc++;
+		
+	}
+
+}
--- apcs/scenes/GameScene.java
+++ apcs/scenes/GameScene.java
@@ -1,6 +1,7 @@
 package apcs.scenes;
 
 import apcs.entitys.Arrow;
+import apcs.entitys.SpriteTest;
 
 import com.tywilly.WillyEngine.Engine;
 import com.tywilly.WillyEngine.scene.Scene;
@@ -8,13 +9,14 @@ import com.tywilly.WillyEngine.scene.Scene;
 public class GameScene extends Scene{
 
 	Arrow arrow = new Arrow(0, Engine.display.getHeight()/2);
+	SpriteTest spriteTest = new SpriteTest(Engine.display.getWidth()/2, Engine.display.getHeight()/4);
 	
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		
-		this.addEntity(arrow);
-		//this.addEntity(new Arrow(Display.SCREEN_WIDTH, Display.SCREEN_HEIGHT/3));
+		//this.addEntity(arrow);
+		this.addEntity(spriteTest);
 		
 	}
 
