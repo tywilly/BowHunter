60c69c2e6f76164e942492878cde2cc5ea97fbec
Tyler Wilcox <tywilly300@gmail.com>
2015-04-26 20:25:25 -0400

    Testing basic input

--- src/apcs/entitys/SpriteTest.java
+++ src/apcs/entitys/SpriteTest.java
@@ -1,26 +1,45 @@
 package apcs.entitys;
 
-import com.tywilly.WillyEngine.Engine;
 import com.tywilly.WillyEngine.entity.sprite.Sprite;
+import com.tywilly.WillyEngine.input.Input;
+import com.tywilly.WillyEngine.input.InputAction;
+import com.tywilly.WillyEngine.input.InputAction.ActionType;
 import com.tywilly.WillyEngine.texture.Texture;
 import com.tywilly.WillyEngine.update.Updateable;
 
-public class SpriteTest extends Sprite implements Updateable{
+public class SpriteTest extends Sprite implements Updateable, Input {
 
 	public SpriteTest(int x, int y) {
-		super(x, y, 64,64, new Texture("assets/obama_sprite_left.png"));
+		super(x, y, 64, 64, new Texture("assets/obama_sprite_left.png"));
 		this.getTexture().loadIntoMemery();
 	}
 
 	@Override
 	public void update(long mili) {
 		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onInput(InputAction e) {
+		// TODO Auto-generated method stub
 		
-		if(xLoc >= Engine.display.getWidth()){
-			xLoc = 0;
+		if(e.getAction() == ActionType.KEYBOARD_DOWN){
+			if(e.getKeyCode() == 'd'){
+				xLoc += 4;
+			}else if(e.getKeyCode() == 'a'){
+				xLoc -= 4;
+			}else if(e.getKeyCode() == 'w'){
+				yLoc -= 4;
+			}else if(e.getKeyCode() == 's'){
+				yLoc += 4;
+			}
+		}else if(e.getAction() == ActionType.MOUSE_DOWN){
+			if(e.getKeyCode() == '1'){
+				xLoc += 2;
+			}else if(e.getKeyCode() == '2'){
+				xLoc -= 2;
+			}
 		}
-		
-		xLoc += 1;
 		
 	}
 
