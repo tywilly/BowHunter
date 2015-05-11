81d95632e421b40d4ab9c7569cc5cc7e1f41bb36
Tyler Wilcox <tywilly300@gmail.com>
2015-05-11 19:07:56 -0400

    Player stuff.

--- src/apcs/entitys/player/Player.java
+++ src/apcs/entitys/player/Player.java
@@ -19,208 +19,168 @@ import com.tywilly.WillyEngine.input.InputAction.ActionType;
 import com.tywilly.WillyEngine.texture.Texture;
 import com.tywilly.WillyEngine.update.Updateable;
 
-public class Player extends Sprite implements Updateable, Input
-{
+public class Player extends Sprite implements Updateable, Input {
 
-    String playerName = "Player 1";
+	String playerName = "Player 1";
 
-    Texture rightTexture;
+	Texture rightTexture;
 
-    Texture leftTexture = new Texture("assets/characters/obama_sprite_left.png");
+	Texture leftTexture = new Texture("assets/characters/obama_sprite_left.png");
 
-    boolean right = false;
+	boolean right = false;
 
-    boolean left = false;
+	boolean left = false;
 
-    boolean up = false;
+	boolean up = false;
 
-    boolean down = false;
+	boolean down = false;
 
-    Inventory inventory = new Inventory();
+	Inventory inventory = new Inventory();
 
-    /*
-     * Skills
-     */
+	/*
+	 * Skills
+	 */
 
-    AttackSkill atackSkill = new AttackSkill();
+	AttackSkill atackSkill = new AttackSkill();
 
-    DefenseSkill defenseSkill = new DefenseSkill();
+	DefenseSkill defenseSkill = new DefenseSkill();
 
-    public Player(int x, int y)
-    {
-        super(x, y, 64, 64, new Texture(
-                "assets/characters/obama_sprite_right.png"));
-        this.getTexture().loadIntoMemery();
-        rightTexture = this.getTexture();
-        leftTexture.loadIntoMemery();
+	public Player(int x, int y) {
+		super(x, y, 64, 64, new Texture(
+				"assets/characters/obama_sprite_right.png"));
+		this.getTexture().loadIntoMemery();
+		rightTexture = this.getTexture();
+		leftTexture.loadIntoMemery();
 
-        inventory.setHandItem(new Bow());
+		inventory.setHandItem(new Bow());
 
-    }
+	}
 
-    @Override
-    public void paint(Graphics g)
-    {
-        // TODO Auto-generated method stub
-        super.paint(g);
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
 
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial", Font.BOLD, 20));
-        g.drawString(playerName, xLoc - 2, yLoc - 10);
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Arial", Font.BOLD, 20));
+		g.drawString(playerName, xLoc - 2, yLoc - 10);
 
-    }
+	}
 
-    @Override
-    public void update(long mili)
-    {
-        // TODO Auto-generated method stub
+	@Override
+	public void update(long mili) {
+		// TODO Auto-generated method stub
 
-        if (up)
-        {
+		if (up) {
 
-            if (yLoc + height / 2 <= Engine.display.getHeight() / 2
-                    && GameScene.world.getYLocation() <= 0)
-            {
-                GameScene.world.moveWorld(0, (int) (0.5 * mili));
-            } else if (yLoc >= 0)
-            {
-                yLoc -= 0.5 * mili;
-            }
+			if (yLoc + height / 2 <= Engine.display.getHeight() / 2
+					&& GameScene.world.getYLocation() <= 0) {
+				GameScene.world.moveWorld(0, (int) (0.5 * mili));
+			} else if (yLoc >= 0) {
+				yLoc -= 0.5 * mili;
+			}
 
-        } else if (down)
-        {
+		} else if (down) {
 
-            if (yLoc + height / 2 >= Engine.display.getHeight() / 2
-                    && -GameScene.world.getYLocation() <= GameScene.world
-                            .getHeight() - Engine.display.getHeight())
-            {
-                GameScene.world.moveWorld(0, -(int) (0.5 * mili));
-            } else if (yLoc + height <= Engine.display.getHeight())
-            {
-                yLoc += 0.5 * mili;
-            }
+			if (yLoc + height / 2 >= Engine.display.getHeight() / 2
+					&& -GameScene.world.getYLocation() <= GameScene.world
+							.getHeight() - Engine.display.getHeight()) {
+				GameScene.world.moveWorld(0, -(int) (0.5 * mili));
+			} else if (yLoc + height <= Engine.display.getHeight()) {
+				yLoc += 0.5 * mili;
+			}
 
-        }
+		}
 
-        if (right)
-        {
+		if (right) {
 
-            if (xLoc + width / 2 >= Engine.display.getWidth() / 2
-                    && -GameScene.world.getXLocation() <= GameScene.world
-                            .getWidth() - Engine.display.getWidth())
-            {
-                GameScene.world.moveWorld((int) -(0.5 * mili), 0);
-            } else if (xLoc + width <= Engine.display.getWidth())
-            {
-                xLoc += 0.5 * mili;
-            }
-        } else if (left)
-        {
+			if (xLoc + width / 2 >= Engine.display.getWidth() / 2
+					&& -GameScene.world.getXLocation() <= GameScene.world
+							.getWidth() - Engine.display.getWidth()) {
+				GameScene.world.moveWorld((int) -(0.5 * mili), 0);
+			} else if (xLoc + width <= Engine.display.getWidth()) {
+				xLoc += 0.5 * mili;
+			}
+		} else if (left) {
 
-            if (xLoc + width / 2 <= Engine.display.getWidth() / 2
-                    && GameScene.world.getXLocation() <= 0)
-            {
-                GameScene.world.moveWorld((int) (0.5 * mili), 0);
-            } else if (xLoc >= 0)
-            {
-                xLoc -= 0.5 * mili;
-            }
-        }
+			if (xLoc + width / 2 <= Engine.display.getWidth() / 2
+					&& GameScene.world.getXLocation() <= 0) {
+				GameScene.world.moveWorld((int) (0.5 * mili), 0);
+			} else if (xLoc >= 0) {
+				xLoc -= 0.5 * mili;
+			}
+		}
 
-    }
+	}
 
-    @Override
-    public void onInput(InputAction e)
-    {
-        // TODO Auto-generated method stub
+	@Override
+	public void onInput(InputAction e) {
+		// TODO Auto-generated method stub
 
-        if (e.getAction() == ActionType.KEYBOARD_DOWN)
-        {
-            if (e.getKeyCode() == 'd')
-            {
-                this.setTexure(rightTexture);
-                right = true;
-            } else if (e.getKeyCode() == 'a')
-            {
-                this.setTexure(leftTexture);
-                left = true;
-            } else if (e.getKeyCode() == 'w')
-            {
-                up = true;
-            } else if (e.getKeyCode() == 's')
-            {
-                down = true;
-            } else if (e.getKeyCode() == ' ')
-            {
-                // shootProjectile(1, 0);
-            }
-        } else if (e.getAction() == ActionType.KEYBOARD_UP)
-        {
+		if (e.getAction() == ActionType.KEYBOARD_DOWN) {
+			if (e.getKeyCode() == 'd') {
+				this.setTexure(rightTexture);
+				right = true;
+			} else if (e.getKeyCode() == 'a') {
+				this.setTexure(leftTexture);
+				left = true;
+			} else if (e.getKeyCode() == 'w') {
+				up = true;
+			} else if (e.getKeyCode() == 's') {
+				down = true;
+			} else if (e.getKeyCode() == ' ') {
+				// shootProjectile(1, 0);
+			}
+		} else if (e.getAction() == ActionType.KEYBOARD_UP) {
 
-            if (e.getKeyCode() == 'd')
-            {
-                right = false;
-            } else if (e.getKeyCode() == 'a')
-            {
-                left = false;
-            } else if (e.getKeyCode() == 'w')
-            {
-                up = false;
-            } else if (e.getKeyCode() == 's')
-            {
-                down = false;
-            }
+			if (e.getKeyCode() == 'd') {
+				right = false;
+			} else if (e.getKeyCode() == 'a') {
+				left = false;
+			} else if (e.getKeyCode() == 'w') {
+				up = false;
+			} else if (e.getKeyCode() == 's') {
+				down = false;
+			}
 
-        } else if (e.getAction() == ActionType.MOUSE_DOWN)
-        {
-            if (e.getKeyCode() == '1')
-            {
+		} else if (e.getAction() == ActionType.MOUSE_DOWN) {
+			if (e.getKeyCode() == '1') {
 
-                // int xDir = (this.xLoc + (this.width/2)) - e.getMouseX();
-                // int yDir = (this.yLoc + (this.height/2)) - e.getMouseY();
+				if (inventory.getHandItem() instanceof Weapon) {
 
-                float xDir = Math.abs(e.getMouseX() - this.xLoc);
-                float yDir = e.getMouseY() - this.yLoc;
+					float xDir = Math.abs(e.getMouseX()
+							- (this.xLoc + this.width / 2));
+					float yDir = e.getMouseY() - (this.yLoc + this.height / 2);
 
-                System.out.println("Mouse:" + xDir + " " + yDir);
+					double angle = Math.atan2(yDir, xDir);
 
-                double angle = Math.atan2(yDir, xDir);
+					if (e.getMouseX() > this.getX()) {
+						xDir = 1;
+					} else if (e.getMouseX() < this.getX()) {
+						xDir = -1;
+					}
 
-                if(e.getMouseX() > this.getX()){
-                    xDir = 1;
-                }else if(e.getMouseX() < this.getX()){
-                    xDir = -1;
-                }
-                
-                yDir = ((float) (xDir * Math.atan(angle)));
-                
-                if(xDir < 0){
-                    yDir = -yDir;
-                }
+					yDir = ((float) (xDir * Math.atan(angle)));
 
-                System.out.println("Arrow:" + xDir + " " + yDir + " Angle: "
-                        + Math.toDegrees(angle));
+					if (xDir < 0) {
+						yDir = -yDir;
+					}
 
-                if (inventory.getHandItem() instanceof Weapon)
-                {
-                    ((Weapon) inventory.getHandItem()).onAction(this, xDir,
-                            yDir);
-                } else if (inventory.getHandItem() instanceof ActionItem)
-                {
-                    ((ActionItem) inventory.getHandItem()).onAction(this);
-                }
+					((Weapon) inventory.getHandItem()).onAction(this, xDir,
+							yDir);
+				} else if (inventory.getHandItem() instanceof ActionItem) {
+					((ActionItem) inventory.getHandItem()).onAction(this);
+				}
 
-            } else if (e.getKeyCode() == '2')
-            {
+			} else if (e.getKeyCode() == '2') {
 
-            }
-        }
+			}
+		}
 
-    }
+	}
 
-    public Inventory getInventory()
-    {
-        return inventory;
-    }
+	public Inventory getInventory() {
+		return inventory;
+	}
 
 }
